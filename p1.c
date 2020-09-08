252edecfd89d1514f10ac62332472508f1551b75
Shubh Agarwal <sazorinos1111@gmail.com>
2020-09-08 16:54:12 +0530

    Trying IF command

---
+++ p1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	//Learning about IF statement
+	printf("Enter a number less than 10:");
+	int q;
+	scanf("%d",&q);
+	 if(q<10)
+		 printf("You are obidient\n");
+	 if(q>=10)
+		 printf("You are not obidient\n");
+	 return 0;
+}
